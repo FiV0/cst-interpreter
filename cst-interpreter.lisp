44217d89d036e08474ade4eb4e36a18ae003df7d
FiV0 <finn.volkel@gmail.com>
2019-07-31 18:41:25 +0200

    little progress

--- cst-interpreter.lisp
+++ cst-interpreter.lisp
@@ -5,10 +5,11 @@
 |#
 
 (defpackage cst-interpreter
-  (:use :cl
-        :eclector.concrete-syntax-tree)
+  (:use :cl)
+  (:import-from :eclector.concrete-syntax-tree
+                :cst-read)
   (:export :repl
-           :evaluate))
+           :cst-evaluate))
 (in-package :cst-interpreter)
 
 (defun repl (&optional (stream *standard-input*))
@@ -16,16 +17,48 @@
   (-> (cst-read stream) cst-evaluate cst-print)
   (repl stream))
 
+(define-condition not-implemented (error) ())
+(define-condition illegal-function-call (error) ())
+
 (defun cst-evaluate (cst)
-  cst)
+  "Evaluates a cst."
+  (if (cst:atom cst)
+      cst
+      (evaluate-compound-form cst)))
+
+(defun evaluate-csts (csts)
+  "Evaluates a list of csts."
+  (mapcar #'evaluate-cst csts))
+
+(defun evaluate-compound-form (cst)
+  (let ((first-raw (-> cst cst:first cst:raw)))
+    ))
+
+(defun evaluate-function-form (cst)
+  (error 'not-implemented))
+
+(defun evaluate-macro-form (cst)
+  (error 'not-implemented))
+
+(defun evaluate-special-form (cst)
+  (error 'not-implemented))
 
 (defun cst-print (cst)
   (format *standard-output* "~a~%" cst))
 
-
 ;; helper stuff
+(defun cst-to-list (cst)
+  "Transforms a cst into a list of csts of its subexpressions."
+  (cond
+    ((cst:null cst)
+     '())
+    ((cst:consp cst)
+     (cons (cst:first cst) (cst-to-list (cst:rest cst))))
+    (T (list cst))))
+
+;; unrelated stuff
 (defmacro -> (x &rest forms)
-  "Thread first macro."
+  "Thread first macro, like in clojure."
   (chain-expander forms x))
 
 (defun chain-expander (forms res)
